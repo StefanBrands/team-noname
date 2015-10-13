bfda42c5da33031ac9f4695a40795e75c4f0db0b
Stefan Brands <sbj@qad.com>
2015-10-13 10:29:14 +0000

    Changes made by Stefan Brands (sbj@qad.com).

--- nonameui/scripts/index.js
+++ nonameui/scripts/index.js
@@ -19,6 +19,26 @@
 var Application;
 (function (Application) {
     function initialize() {
+        var sugList = $("#suggestions");
+
+        $("#searchField").on("input", function (e) {
+            var text = $(this).val();
+            if (text.length < 1) {
+                sugList.html("");
+                sugList.listview("refresh");
+            } else {
+                $.get("service.cfc?method=getSuggestions", { search: text }, function (res, code) {
+                    var str = "";
+                    for (var i = 0, len = res.length; i < len; i++) {
+                        str += "<li>" + res[i] + "</li>";
+                    }
+                    sugList.html(str);
+                    sugList.listview("refresh");
+                    console.dir(res);
+                }, "json");
+            }
+        });
+
         document.addEventListener('deviceready', onDeviceReady, false);
     }
     Application.initialize = initialize;
--- nonameui/scripts/index.ts
+++ nonameui/scripts/index.ts
@@ -18,6 +18,28 @@
  */
 module Application {
     export function initialize() {
+        
+        var sugList = $("#suggestions");
+
+        $("#searchField").on("input", function(e) {
+             var text = $(this).val();
+             if(text.length < 1) {
+                 sugList.html("");
+                 sugList.listview("refresh");
+             } else {
+                 $.get("service.cfc?method=getSuggestions", {search:text}, function(res,code) {
+                     var str = "";
+                     for(var i=0, len=res.length; i<len; i++) {
+                         str += "<li>"+res[i]+"</li>";
+                     }
+                     sugList.html(str);
+                     sugList.listview("refresh");
+                     console.dir(res);
+                 },"json");
+             }
+         });
+
+        
         document.addEventListener('deviceready', onDeviceReady, false);
     }
 
